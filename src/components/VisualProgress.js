a8759867d010164fc35eb1701f0df7b73a5eb5e9
Lianne van Westrhenen <lianne.van.westrhenen@gmail.com>
2018-01-11 11:08:39 +0100

    visual progress

--- src/components/VisualProgress.js
+++ src/components/VisualProgress.js
@@ -7,12 +7,18 @@ class VisualProgress extends PureComponent {
 
   renderHangMan() {
     if ((this.props.visualProgress) === 0) {
-      return <img className="crossFading" src='http://www.justhangman.com/images/hangman_0.gif' alt = "0"/>
+      return <img className="image" src='http://www.animatedimages.org/data/media/712/animated-number-image-0200.gif' alt = "0"/>
     } else if ((this.props.visualProgress) === 1) {
-      return <img className="crossFading" src='http://www.sightwordsgame.com/wp-content/uploads/2011/11/Hangman.jpg' alt="1"/>
+      return <img className="image" src='https://cdn.dribbble.com/users/105681/screenshots/2763547/5.gif' alt="1"/>
     } else if ((this.props.visualProgress) === 2) {
-      return <img className="crossFading" src='https://vignette.wikia.nocookie.net/austinally/images/c/ce/Hangman_game.png/revision/latest?cb=20140725033826' alt="2"/>
+      return <img className="image" src='http://www.animatedimages.org/data/media/712/animated-number-image-0198.gif' alt="2"/>
     } else if ((this.props.visualProgress) === 3) {
+      return <img className="image" src='http://moziru.com/images/number-clipart-animated-11.gif' alt="3"/>
+    } else if ((this.props.visualProgress) === 4) {
+      return <img className="image" src='https://thumbs.gfycat.com/NaiveFluffyGadwall-small.gif' alt="4"/>
+    } else if ((this.props.visualProgress) === 5) {
+      return <img className="image" src='https://gifimage.net/wp-content/uploads/2017/10/number-1-gif-12.gif' alt="5"/>
+    } else if ((this.props.visualProgress) === 6) {
       return <GameOver />
     } else {
       return null
